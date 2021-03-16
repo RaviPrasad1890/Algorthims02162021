34ad83286f1b405b956f2b359d33e8deb11417b5
Ravi Prasad <ravi.prasad.1890@gmail.com>
2021-03-16 16:22:57 +0530

    Doing some basics

--- src/com/dynamicprogramming/meenakshi_kamalRawat/Recurssion/SumOfNnumber.java
+++ src/com/dynamicprogramming/meenakshi_kamalRawat/Recurssion/SumOfNnumber.java
@@ -1,6 +1,6 @@
-package com.dynamicprogramming.meenakshi_kamalRawat;
+package com.dynamicprogramming.meenakshi_kamalRawat.Recurssion;
 //Sum of n natural numbers
-public class Sum {
+public class SumOfNnumber {
     public static int getSum(int n){
         return n==0?0:n==1?1:n+getSum(n-1);
     }
